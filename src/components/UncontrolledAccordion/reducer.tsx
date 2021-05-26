9145a10748349f5400a4553c73924fec80bd43c4
Moon <m.p.skuratova@gmail.com>
2021-05-26 17:39:32 +0600

    16  занятий

--- src/components/UncontrolledAccordion/reducer.tsx
+++ src/components/UncontrolledAccordion/reducer.tsx
@@ -1,3 +1,5 @@
+
+
 type ActionType = {
     type: string
 }
@@ -13,3 +15,22 @@ export const reducer = (state: StateType, action: ActionType):StateType => {
     }
     return state;
 }
+
+let list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+function isSameLanguage(list: any) {
+    // thank you for checking out the Coding Meetup kata :)
+    let lang = list.language[0]
+    for (let i = 0; i < list.length; i++) {
+        if (list.language[i] === lang) {
+            return true
+        }
+        return false
+    }
+}
+
+console.log(isSameLanguage(list1))
